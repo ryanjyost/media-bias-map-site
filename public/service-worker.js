4a4d88dba8bbc79b4dc46ab8794888e95cc6d89e
Ryan Yost <ryost217@gmail.com>
2018-07-01 18:07:36 -0500

    err typo

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,20 @@
 var CACHE_NAME = "birds-eye-view-v1";
 var urlsToCache = ["/", "/static/js/bundle.js"];
+
 self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       // Open a cache and cache our files
       return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
