52998fd888e4dca49a1cd689dc78e19057b6c0fe
Ryan Yost <ryost217@gmail.com>
2018-07-01 19:03:04 -0500

    force https

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import HttpsRedirect from "react-https-redirect";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <HttpsRedirect>
+    <App />
+  </HttpsRedirect>,
+  document.getElementById("root")
+);
 //registerServiceWorker();
